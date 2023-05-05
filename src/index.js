62caead6f0ed618ee91395431d8b18c0c792d804
tuogn2 <95692889+tuogn2@users.noreply.github.com>
2023-05-05 22:35:30 +0700

    hi

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(express.static(path.join(__dirname,'public')))
 
-app.use(cookieParser())
+app.use(cookieParser('dsadassfsdf'))
 
 db.connect(); 
 app.use(express.urlencoded({
