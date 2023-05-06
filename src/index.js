573bdc216eef67f133bfea9693b9b29ddc04a500
tuogn2 <95692889+tuogn2@users.noreply.github.com>
2023-05-06 22:40:24 +0700

    hi

--- src/index.js
+++ src/index.js
@@ -55,8 +55,8 @@ app.set('views',  path.join(__dirname, 'resources\\views'));
 
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Credentials', true);
-  // res.setHeader('Access-Control-Allow-Origin','https://main--zingy-pixie-868efb.netlify.app');
-  res.setHeader('Access-Control-Allow-Origin','http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin','https://main--zingy-pixie-868efb.netlify.app');
+  // res.setHeader('Access-Control-Allow-Origin','http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
